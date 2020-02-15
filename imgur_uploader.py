756bccbdce3a63a625012cb5caea3cd2c1a9c618
Jack Henhapl <jackah2@illinois.edu>
2020-02-15 01:36:22 -0600

    Fixed Imgur uploader

--- imgur_uploader.py
+++ imgur_uploader.py
@@ -4,28 +4,46 @@ import webbrowser
 import secrets
 from configparser import ConfigParser
 
-def auth_with_pin(client):
+def set_config_val(config_parser, key, val):
+    set_config_val_dict(config_parser, {key: val})
+
+def set_config_val_dict(config_parser, arg_dict):
+    for key, val in arg_dict.items():
+        config_parser.set('imgur', key, val)
+    with open('config.ini', 'w') as file:
+        config_parser.write(file)
+
+def get_config_val(config_parser, key, fallback=None):
+    return config_parser.get('imgur', key, fallback=fallback)
+
+def get_config_val_dict(config_parser, *args):
+    return { key : get_config_val(config_parser, key) for key in args }
+
+def auth_with_pin(client, config_parser):
     auth_url = client.authorization_url('pin')
     webbrowser.open(auth_url)
-    pin = input('What is the pin?')
+    pin = input('What is the pin: ')
     response = client.exchange_pin(pin)
-    print(response)
-#   setter.set_authentication(new_access_token = response[0], new_refresh_token = response[1])
+
+    set_config_val_dict(config_parser, {
+        'access_token': response[0],
+        'refresh_token': response[1],
+    })
+
     return response
 
 
-def upload_to_imgur(img_path, img_name, override=False):
-    """Uploads an image to Imgur.
+def upload_to_imgur(path_name_dict, override=False):
+    """Uploads a bunch of images to Imgur.
     
     Arguments:
-        img_path {str} -- Path to an image to upload
-        img_name {str} -- Name of the picture once uploaded to Imgur
+        path_name_dict {dict} -- (image path, image name) tuple dictionary
     
     Keyword Arguments:
         override {bool} -- If an image with the same name already exists, should it be overwritten? (default: {False})
     
     Returns:
-        [str] -- Imgur link to uploaded image
+        [list] -- List of Imgur links to uploaded images
     """
 
     print('\n\nGetting Imgur client...')
@@ -33,67 +51,67 @@ def upload_to_imgur(img_path, img_name, override=False):
     parser = ConfigParser()
     parser.read('config.ini')
 
-    client = pyimgur.Imgur(
-        client_id=secrets.IMGUR_CLIENT_ID,
-        client_secret=secrets.IMGUR_CLIENT_SECRET,
-    )
-
-    if not secrets.IMGUR_CLIENT_ID or not secrets.IMGUR_CLIENT_SECRET:
-        print('Please enter the IMGUR_CLIENT_ID and IMGUR_CLIENT_SECRET into `config.ini`!')
+    if not parser.get('imgur', 'client_id') or not parser.get('imgur', 'client_secret'):
+        print('Please enter the `client_id` and `client_secret` into `config.ini`!')
         exit()
 
-    if not secrets.IMGUR_REFRESH_TOKEN:
-        print('IMGUR_REFRESH_TOKEN does not exist within `secrets.py`')
-        auth_with_pin(client)
-
-        client = pyimgur.Imgur(
-            client_id=secrets.IMGUR_CLIENT_ID,
-            client_secret=secrets.IMGUR_CLIENT_SECRET,
-            refresh_token=secrets.IMGUR_REFRESH_TOKEN
-        )
+    if get_config_val(parser, 'refresh_token'):
+        client = pyimgur.Imgur(**get_config_val_dict(parser, 
+            'client_id', 'client_secret', 'refresh_token'
+        ))
+    else:
+        client = pyimgur.Imgur(**get_config_val_dict(parser, 
+            'client_id', 'client_secret'
+        ))
+        auth_with_pin(client, parser)
 
     try:
         print('Refreshing access token')
-        client.refresh_access_token()
+        access_token = client.refresh_access_token()
+        set_config_val(parser, 'access_token', access_token)
     except:
         print('Access token failed to refresh')
-        response = auth_with_pin(client)
-        client = pyimgur.Imgur(
-            client_id=secrets.IMGUR_CLIENT_ID,
-            client_secret=secrets.IMGUR_CLIENT_SECRET,
-            refresh_token=secrets.IMGUR_REFRESH_TOKEN
-        )
+        auth_with_pin(client, parser)
+        client = pyimgur.Imgur(**get_config_val_dict(parser, 
+            'client_id', 'client_secret', 'refresh_token'
+        ))
 
     print('Client retrieved')
 
-    username = secrets.IMGUR_USERNAME
-    album_id = secrets.IMGUR_ALBUM_ID
+    username = get_config_val(parser, 'username')
+    album_id = get_config_val(parser, 'album_id')
 
-    if not secrets.IMGUR_USERNAME:
+    if not username:
         username = input('What is the username?')
-        # setter.set_credentials(new_username=username)
-    if not secrets.IMGUR_ALBUM_ID:
+        set_config_val(parser, 'username', username)
+    if not album_id:
         album_id = input('What is the album id?')
-        # setter.set_credentials(new_album_id=album_id)
+        set_config_val(parser, 'album_id', album_id)
 
     user = client.get_user(username)
     album = client.get_album(album_id)
 
-    for image in album.images:
-        if img_name == image.title:
-            if override:
-                print('Overriding pre-existing image!')
-                album.remove_images(image)
-            else:
-                print('Pre-existing image found!')
-                print('%s: %s' % (image.title, image.link))
-                return image.link
-                exit()
+    links = []
+    for img_path, img_name in path_name_dict.items():
 
-    print('Uploading image...')
-    image = client.upload_image(path=img_path, title=img_name)
-    album.add_images(image)
-    print('Image uploaded!')
-    print('%s: %s' % (image.title, image.link))
+        found = False
+        for image in album.images:
+            if img_name == image.title:
+                if override:
+                    print('Overriding pre-existing image!')
+                    album.remove_images(image)
+                else:
+                    print('FOUND %s: %s' % (image.title, image.link))
+                    links.append(image.link)
+                    
+                    found = True
+                    break
+        if found:
+            continue
 
-    return image.link
+        image = client.upload_image(path=img_path, title=img_name)
+        album.add_images(image)
+        links.append(image.link)
+        print('UPLOADED %s: %s' % (image.title, image.link))
+
+    return links
