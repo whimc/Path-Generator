6d5aa99ef2bdf240d1c807bcab4a0481ffa1ed93
Jack Henhapl <jackah2@illinois.edu>
2021-04-05 09:45:42 -0500

    Exit if config values aren't set

--- pathgenerator/config.py
+++ pathgenerator/config.py
@@ -8,6 +8,12 @@ _parser = RawConfigParser()
 _parser.optionxform = str
 _parser.read(PATH)
 
+for section, items in _parser.items():
+    unset = [key for key, item in items.items() if not item]
+    for key in unset:
+        print(f'{section}.{key} is not set within the config!')
+exit()
+
 TABLES_SECTION = 'database-tables'
 _get = lambda entry: _parser.get(TABLES_SECTION, entry)
 BLOCKS_TABLE = _get('coreprotect_blocks')
