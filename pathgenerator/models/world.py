7ea7eaa2c480807f51902507bfacc5b15ff6bde1
Jack Henhapl <jackah2@illinois.edu>
2021-05-13 22:33:15 -0500

    Making ratio float

--- pathgenerator/models/world.py
+++ pathgenerator/models/world.py
@@ -6,6 +6,6 @@ class World:
     world_name: str
     image_path: str
     coreprotect_id: int
-    pixel_to_block_ratio: int = 1
+    pixel_to_block_ratio: float = 1.0
     top_left_coordinate_x: int = -512
     top_left_coordinate_z: int = -512
